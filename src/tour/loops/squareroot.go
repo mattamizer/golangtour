61925d31b067844ef440898c96b7063b672352f0
Matthew Morrissey <matthew.morrissey@sungardas.com>
2014-12-06 12:38:10 -0500

    Change the conditional and break out of the loop at delta < 1e-10

--- src/tour/loops/squareroot.go
+++ src/tour/loops/squareroot.go
@@ -2,16 +2,23 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func Sqrt(num float64) float64 {
 	z := 1.0
-	for i := 1; i <= 10; i++ {
-		z = z - ((z*z - num) / 2 * z)
+	delta := 0.0
+	for {
+		z = z - (z*z-num)/(2*z)
+		if math.Abs(delta-z) < 1e-10 {
+			break
+		}
+		delta = z
 	}
-	return z
+	return delta
 }
 
 func main() {
 	fmt.Println(Sqrt(2))
+	fmt.Println(math.Sqrt(2))
 }
